09cb216f47d9807186ab11ca9c2944963bd2a8e4
susuhaohaki <nguyenhai250203@gmail.com>
2024-10-12 16:57:30 +0700

    update footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <div className="bg-white dark:bg-dark text-gray-700 dark:text-gray-300">
       <div className="flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm">
-        {/* footer left */}
+        {/* Footer left */}
         <div>
           <Link to="/">
             <h1 className="text-5xl">Shoes</h1>
@@ -17,27 +17,37 @@ const Footer = () => {
             and scrambled it to make a type specimen book.
           </p>
         </div>
-        {/* footer center */}
+
+        {/* Footer center */}
         <div>
           <p className="text-xl font-medium mb-5">COMPANY</p>
           <ul className="flex flex-col gap-1">
-            <li>HOME</li>
-            <li>ABOUT</li>
-            <li>DELIVERY</li>
-            <li>PRIVACY POLICY</li>
+            <li>
+              <Link to="/">HOME</Link>
+            </li>
+            <li>
+              <Link to="/about">ABOUT</Link>
+            </li>
+            <li>
+              <a href="#!">DELIVERY</a>
+            </li>
+            <li>
+              <a href="#!">PRIVACY POLICY</a>
+            </li>
           </ul>
         </div>
-        {/* footer right  */}
+
+        {/* Footer right */}
         <div>
           <p className="text-xl font-medium mb-5">GET IN TOUCH</p>
           <ul className="flex flex-col gap-1">
             <li>+84 000-000-0000</li>
             <li>nguyenhai250203@gmail.com</li>
-            <li></li>
           </ul>
         </div>
       </div>
-      {/* copyright  */}
+
+      {/* Copyright */}
       <div>
         <hr className="border-gray-300 dark:border-gray-700" />
         <p className="py-5 text-sm text-center">
